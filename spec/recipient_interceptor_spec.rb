a23321650403ed2aa486d1ffc538b9fbf7963517
Dan Croak <dan@thoughtbot.com>
2013-03-11 23:20:16 -0400

    Fix specs for Ruby 2.0

--- spec/recipient_interceptor_spec.rb
+++ spec/recipient_interceptor_spec.rb
@@ -2,19 +2,19 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'recipient_interceptor')
 
 describe RecipientInterceptor do
   it 'overrides to/cc/bcc fields, copies original fields to custom headers' do
-    Mail.register_interceptor RecipientInterceptor.new('staging@example.com')
+    Mail.register_interceptor RecipientInterceptor.new(recipient_string)
 
     response = deliver_mail
 
-    expect(response.to).to eq ['staging@example.com']
+    expect(response.to).to eq [recipient_string]
     expect(response.cc).to eq []
     expect(response.bcc).to eq []
 
-    expect(response.header['X-Intercepted-To'].to_s).
-      to eq "[original.to@example.com, staging@example.com]"
-    expect(response.header['X-Intercepted-Cc'].to_s).
+    expect(custom_header(response, 'X-Intercepted-To')).
+      to eq ['original.to@example.com', 'staging@example.com']
+    expect(custom_header(response, 'X-Intercepted-Cc')).
       to eq 'original.cc@example.com'
-    expect(response.header['X-Intercepted-Bcc'].to_s).
+    expect(custom_header(response, 'X-Intercepted-Bcc')).
       to eq 'original.bcc@example.com'
   end
 
@@ -41,7 +41,21 @@ describe RecipientInterceptor do
     mail.deliver!
   end
 
+  def recipient_string
+    'staging@example.com'
+  end
+
   def recipient_array
     ['one@example.com', 'two@example.com']
+  end
+
+  def custom_header(response, name)
+    header = response.header[name]
+
+    if header.respond_to?(:map)
+      header.map {|header| header.value.wrapped_string }
+    else
+      header.to_s
+    end
   end
 end
