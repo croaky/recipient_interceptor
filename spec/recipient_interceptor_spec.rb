eca80410c3e36815da569e2fd13310a28a423602
Uri Gorelik <uri.gore@gmail.com>
2018-04-12 17:07:12 -0400

    Setting cc and bcc fields to nil instead of []
    This was causing a problem with a certain `ActionMailer` provider
    (Mailgun/Railgun).
    
    This should also be the more canonical approach to un-setting these
    fields as by default they are `nil`.
    
    Setting the `Mail::Message`'s header differently was also required. The
    previous approach would encode the message's headers to a string and
    then append another encoded string to create the new headers. However,
    the `bcc` field is not encoded by default
    (see `Mail::BccField#encoded` @ 2.7.0).
    
    Since the `bcc` field is not present in the encoded version of the
    headers, it is effectively deleted and set to `[]`. Due to how setting
    headers works in Mail, `message.bcc = nil` will not actually work.
    
    By using `Mail::Header[]=` instead of `Mail::Header=`,
    we do not clear the `bcc` field and can properly use
    `message.bcc = nil` to remove it.

--- spec/recipient_interceptor_spec.rb
+++ spec/recipient_interceptor_spec.rb
@@ -1,14 +1,37 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'recipient_interceptor')
 
 describe RecipientInterceptor do
+  before do
+    Mail.defaults do
+      delivery_method :test
+    end
+
+    module Mail
+      @@delivery_interceptors = []
+    end
+  end
+
   it 'overrides to/cc/bcc fields' do
     Mail.register_interceptor RecipientInterceptor.new(recipient_string)
 
     response = deliver_mail
 
     expect(response.to).to eq [recipient_string]
-    expect(response.cc).to eq []
-    expect(response.bcc).to eq []
+    expect(response.cc).to eq nil
+    expect(response.bcc).to eq nil
+  end
+
+  it 'overrides to/cc/bcc correctly even if they were already missing' do
+    Mail.register_interceptor RecipientInterceptor.new(recipient_string)
+
+    response = Mail.deliver do
+      from 'original.from@example.com'
+      to 'original.to@example.com'
+    end
+
+    expect(response.to).to eq [recipient_string]
+    expect(response.cc).to eq nil
+    expect(response.bcc).to eq nil
   end
 
   it 'copies original to/cc/bcc fields to custom headers' do
@@ -16,12 +39,12 @@ describe RecipientInterceptor do
 
     response = deliver_mail
 
-    expect(custom_header(response, 'X-Intercepted-To')).
-      to eq 'original.to@example.com'
-    expect(custom_header(response, 'X-Intercepted-Cc')).
-      to eq 'original.cc@example.com'
-    expect(custom_header(response, 'X-Intercepted-Bcc')).
-      to eq 'original.bcc@example.com'
+    expect(custom_header(response, 'X-Intercepted-To'))
+      .to eq 'original.to@example.com'
+    expect(custom_header(response, 'X-Intercepted-Cc'))
+      .to eq 'original.cc@example.com'
+    expect(custom_header(response, 'X-Intercepted-Bcc'))
+      .to eq 'original.bcc@example.com'
   end
 
   it 'accepts an array of recipients' do
@@ -68,10 +91,6 @@ describe RecipientInterceptor do
   end
 
   def deliver_mail
-    Mail.defaults do
-      delivery_method :test
-    end
-
     Mail.deliver do
       from 'original.from@example.com'
       to 'original.to@example.com'
@@ -88,12 +107,6 @@ describe RecipientInterceptor do
       header.map { |h| h.value.wrapped_string }
     else
       header.to_s
-    end
-  end
-
-  after do
-    module Mail
-      @@delivery_interceptors = []
     end
   end
 end
